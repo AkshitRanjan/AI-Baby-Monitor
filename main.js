53add3fc7866580bb86b37921afc788007cd3a35
AkshitRanjan <73226453+AkshitRanjan@users.noreply.github.com>
2021-05-18 13:46:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+img = "";
+status = "";
+
+objects = [];
+
+function preload() {
+    alarm = loadSound("alarm.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+
+    ObjectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"; 
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        ObjectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            if (objects[i].label == "person") {
+                document.getElementById("baby_found_status").innerHTML = "Baby Found";
+                alarm.stop();   
+            }
+            else {
+                document.getElementById("baby_found_status").innerHTML = "Baby Not Found";
+                alarm.play();
+            }
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+
+            text(objects[i].label + " " + percent + " %" + objects[i].x , objects[i].y);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+
+        if (objects[i].length < 0) {
+            document.getElementById("baby_found_status").innerHTML = "Baby Not Found";
+            alarm.play();
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
